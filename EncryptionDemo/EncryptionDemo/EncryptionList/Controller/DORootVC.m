d8ae73f2283387cf6de488c3324ad7182289f4f4
Dino <kill_death@163.com>
2018-05-01 13:41:12 +0800

    Demo初步搭建

--- EncryptionDemo/EncryptionDemo/EncryptionList/Controller/DORootVC.m
+++ EncryptionDemo/EncryptionDemo/EncryptionList/Controller/DORootVC.m
@@ -7,18 +7,40 @@
 //
 
 #import "DORootVC.h"
+#import "DOEncryptionListTableView.h"
 
 @interface DORootVC ()
+
+@property (nonatomic, strong) DOEncryptionListTableView *list_tableView;
 
 @end
 
 @implementation DORootVC
 
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.title = @"iOS常用加密算法";
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self configSubViews];
+}
+
+#pragma mark - Custom Cycle
+- (void)configSubViews
+{
+    [self.view addSubview:self.list_tableView];
+}
+
+#pragma mark - Getter Cycle
+- (DOEncryptionListTableView *)list_tableView
+{
+    if (!_list_tableView)
+    {
+        _list_tableView = [[DOEncryptionListTableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    }
+    return _list_tableView;
 }
 
 @end
---
+++ EncryptionDemo/EncryptionDemo/EncryptionList/View/DOEncryptionListTableView.h
@@ -0,0 +1,13 @@
+//
+//  DOEncryptionListTableView.h
+//  EncryptionDemo
+//
+//  Created by 魏欣宇 on 2018/5/1.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DOBaseTableView.h"
+
+@interface DOEncryptionListTableView : DOBaseTableView
+
+@end
