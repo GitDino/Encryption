549190b0daf8bf0d15b42997723ad3f1225bc992
Dino <kill_death@163.com>
2018-05-01 13:12:00 +0800

    Demo初步搭建

---
+++ EncryptionDemo/EncryptionDemo/Controller/DORootVC.h
@@ -0,0 +1,13 @@
+//
+//  DORootVC.h
+//  EncryptionDemo
+//
+//  Created by 魏欣宇 on 2018/5/1.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DORootVC : UIViewController
+
+@end
---
+++ EncryptionDemo/EncryptionDemo/Controller/DORootVC.m
@@ -0,0 +1,24 @@
+//
+//  DORootVC.m
+//  EncryptionDemo
+//
+//  Created by 魏欣宇 on 2018/5/1.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"iOS常用加密算法";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
